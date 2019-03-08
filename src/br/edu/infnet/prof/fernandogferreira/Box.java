566d64992ded30f0a79be7d73932d2bccda1a315
cloudera <cloudera@quickstart.cloudera>
2019-03-07 16:47:41 -0800

    Exceptions treated when user does not write float numbers, and nicer output

--- src/br/edu/infnet/prof/fernandogferreira/Box.java
+++ src/br/edu/infnet/prof/fernandogferreira/Box.java
@@ -1,7 +1,8 @@
 package br.edu.infnet.prof.fernandogferreira;
 
 import java.util.Scanner;
-
+import java.util.InputMismatchException;
+ 
 public class Box {
 	float depth, height, width;
 
@@ -20,26 +21,60 @@ public class Box {
 
 	
 	public float calculateVolume(){
-		return this.depth * this.height * this.width;
+		return (this.depth * 
+				this.height * 
+				this.width);
+	}
+	
+	public String formatVolume(float volume){
+		return String.format("%.2f", volume);
 	}
 
 	public static void main(String[] args) {
 		Box bobj = new Box();
 		Scanner scan = new Scanner(System.in);
+		float w = 0;
+		// Box width
 		System.out.println("Qual a largura da caixa?");
-		float width = scan.nextFloat();
-		bobj.setWidth(width);
-		System.out.println("Usuario escolhou largura: " + width + " cm"  );
+		try {
+			w = scan.nextFloat();
+			bobj.setWidth(w);
+			System.out.println("Usuario escolhou largura: " + w + " m"  );
+		} catch (InputMismatchException exception){
+			System.out.println("O programa espera um numero valido" +
+		                       " para a largura da caixa");
+			scan.close();
+			return;
+		}
+		// Box Height
 		System.out.println("Qual a altura da caixa?");
-		float height = scan.nextFloat();
-		bobj.setHeight(height);
-		System.out.println("Usuario escolhou altura: " + height + " cm"  );
-		System.out.println("Qual a profundidade da caixa?");
-		float depth = scan.nextFloat();
-		bobj.setDepth(depth);
-		System.out.println("Usuario escolhou profundidade: " + depth + " cm"  );
+		try {
+			float h = scan.nextFloat();
+			bobj.setHeight(h);
+			System.out.println("Usuario escolhou altura: " + h + " m"  );
+		} catch (InputMismatchException exception){
+			System.out.println("O programa espera um numero valido" +
+		                       " para a altura da caixa");
+			scan.close();
+			return;
+		}
+		// Box Depth
+		try{
+			System.out.println("Qual a profundidade da caixa?");
+			float d = scan.nextFloat();
+			bobj.setDepth(d);
+		System.out.println("Usuario escolhou profundidade: " + d + " m"  );
+		} catch (InputMismatchException exception){
+			System.out.println("O programa espera um numero valido" +
+							    " para a profundidade da caixa");
+			scan.close();
+			return;
+		}
 		
-		System.out.println("O volume da caixa eh " + bobj.calculateVolume() + "cm3" );
+		System.out.println("O volume da caixa eh " 
+		                   +  bobj.formatVolume(bobj.calculateVolume()) 
+				           + " m3" );
+		scan.close();
 	}
 
 }
